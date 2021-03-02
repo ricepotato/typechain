cb68bf974067d422c4923111acdeea54c096717c
ricepotato <ricepotato40@gmail.com>
2021-03-03 03:52:20 +0900

    blockchain 2

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,48 @@
+import * as CryptoJs from "crypto-js";
+
 class Block {
-    public index:number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-    constructor(index:number,
-        hash: string,
-        previousHash: string,
-        data: string,
-        timestamp: number) {
-            this.index = index;
-            this.hash = hash;
-            this.previousHash = previousHash;
-            this.data = data;
-            this.timestamp = timestamp;        
-    }
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+  };
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
+const genesisBlock: Block = new Block(0, "202020", "", "hello", 12345);
 
-const genesisBlock: Block = new Block(0, "202020", "", "hello", 12345); 
-
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
 console.log(blockchain);
 
-// blockchain.push("stuff");  string type push 불가
+const getBlockchain = (): Block[] => blockchain;
 
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+// blockchain.push("stuff");  string type push 불가
 
 export {}; // typescript rule, 이 파일이 모듈이 된다는 의미.
