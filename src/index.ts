71bfdb933483adb289d57be3352f94dfc0c65fdd
ricepotato <ricepotato40@gmail.com>
2021-03-03 03:35:51 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,20 @@ interface Human {
     gender: string;
 }
 
+// ts 에서는 property 를 명시해야 한다.
+ class HumanClsss {
+     public name: string;
+     public age: number;
+     public gender: string;
+
+     constructor(name:string, age:number, gender:string){         
+         this.name = name;
+         this.age = age;
+         this.gender = gender;
+     }
+
+ }
+
 const name = "sukjun";
 const age = 24;
 const gender = "male";
@@ -13,6 +27,8 @@ const person = {
     gender: "male",
     age: 22
 }
+
+const sukjun = new HumanClsss("sukjun", 24, "male");
 
 // parameter 뒤에 ? 붙이면 optional
 const sayHi = (name, age, gender?) => {
@@ -28,8 +44,14 @@ const sayHi3 = (person: Human): string => {
     return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
+const sayHi4 = (person: HumanClsss): string => {
+    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+
 sayHi(name, age);
 sayHi2(name, age, gender);
 console.log(sayHi3(person));
+console.log(sayHi4(sukjun));
 
 export {}; // typescript rule, 이 파일이 모듈이 된다는 의미.
